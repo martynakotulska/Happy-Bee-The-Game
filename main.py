dd852b919994f9a1beb6d8e46a85be49e1194413
Martyna Kotulska <mkotulska98@gmail.com>
2020-07-03 22:21:42 +0200

    Add key controls

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import random
 
 #Intialize the pygame
 pygame.init()
 
 #Create the screen
 screen = pygame.display.set_mode((800, 600))
+
+#Background
+background = pygame.image.load('background.jpg')
 
 #Title and Icon
 pygame.display.set_caption("Happy Bee")
@@ -15,22 +19,60 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+#Enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 200)
+enemyX_change = 0.25
+enemyY_change = 40
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 #Game Loop
 running = True
 while running:
     # RGB - Red, Green, Blue
     screen.fill((225, 225, 0))
-
+    #Bacground Image
+    screen.blit(background,(0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        #If keystroke is pressed check whether its right / left or up / down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = +0.3
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
+    #Boundaries for bee
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
 
-    player()
+    #Enemy movement
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.25
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.25
+        enemyY += enemyY_change
+
+    player(playerX,playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
